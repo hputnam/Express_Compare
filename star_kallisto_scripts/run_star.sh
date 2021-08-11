8655f874f818f59d82b48806de69b982f4cba636
Nitya N <84518640+nnadgi01@users.noreply.github.com>
2021-08-11 15:43:48 -0400

    Update run_star.sh

--- star_kallisto_scripts/run_star.sh
+++ star_kallisto_scripts/run_star.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 #SBATCH --mem=64G
-#SBATCH --array=54-65
-#SBATCH -n 10
 
-# Separate by species
+ids=( "SRR15089754" "SRR15089755" "SRR15089756" "SRR15089757" "SRR15089758" "SRR15089759" "SRR15089760" "SRR15089761" "SRR15089762" "SRR15089763" "SRR15089764" "SRR15089765" )
 
-module load STAR/2.5.3a-foss-2016b
+length=${#ids[@]}
 
-if ((${SLURM_ARRAY_TASK_ID} > 54)) && ((${SLURM_ARRAY_TASK_ID} < 65)) && ((${SLURM_ARRAY_TASK_ID} !=  54)) && ((${SLURM_ARRAY_TASK_ID} != 57)) && ((${SLURM_ARRAY_TASK_ID} != 58)) && ((${SLURM_ARRAY_TASK_ID} != 59)) && ((${SLURM_ARRAY_TASK_ID} != 60));
-then
-    GENOME=/path/to/annotations/pacuta.gff
-else
-    GENOME=/path/to/annotations/mcap.gff
-fi
-
-STAR \
---runMode alignReads \
---quantMode TranscriptomeSAM \
---outTmpDir /path/to/temp/dir/${SLURM_ARRAY_TASK_ID}_TMP \
---readFilesIn /path/to/fastq/trimmed/SRR150897${SLURM_ARRAY_TASK_ID}_1P /path/to/fastq/trimmed/SRR150897${SLURM_ARRAY_TASK_ID}_2P  \
---genomeDir /r/corals/Nitya/pipelines/star_output/genome_indices/ \
---sjdbGTFfeatureExon exon \
---sjdbGTFtagExonParentTranscript Parent \
---sjdbGTFfile ${GENOME} \
---twopassMode Basic \
---twopass1readsN -1 \
---outStd Log BAM_Unsorted BAM_Quant \
---outSAMtype BAM Unsorted \
---outReadsUnmapped Fastx \
---outFileNamePrefix /path/to/out/aligned_reads/SRR150897${SLURM_ARRAY_TASK_ID}
+for (( i=0; i<length; i++ ))
+do
+    # Separate by species
+    if ((i != 1)) && ((i != 2)) && ((i != 7)) && ((i != 8)) && ((i != 9)) && ((i != 10)); 
+    then
+        STAR \
+        --runMode alignReads \
+        --quantMode TranscriptomeSAM \
+        --outTmpDir /r/corals/Nitya/pipelines/star_output/aligned_reads/${ids[i]} \
+        --readFilesIn /r/corals/Nitya/pipelines/fastq/trimmed/${ids[i]}_1P /r/corals/Nitya/pipelines/fastq/trimmed/${ids[i]}_2P  \
+        --genomeDir /r/corals/Nitya/pipelines/star_output/genome_indices \
+        --sjdbGTFfeatureExon exon \
+        --sjdbGTFtagExonParentTranscript Parent \
+        --sjdbGTFfile /r/corals/Nitya/pipelines/annotations/Mcap.exons.GFFannotation.gff \
+        --twopassMode Basic \
+        --twopass1readsN -1 \
+        --outStd Log BAM_Unsorted BAM_Quant \
+        --outSAMtype BAM Unsorted \
+        --outReadsUnmapped Fastx \
+        --outFileNamePrefix /r/corals/Nitya/pipelines/star_output/aligned_reads/${ids[i]}
+    else
+        STAR \
+        --runMode alignReads \
+        --quantMode TranscriptomeSAM \
+        --outTmpDir /r/corals/Nitya/pipelines/star_output/aligned_reads/${ids[i]} \
+        --readFilesIn /r/corals/Nitya/pipelines/fastq/trimmed/${ids[i]}_1P /r/corals/Nitya/pipelines/fastq/trimmed/${ids[i]}_2P  \
+        --genomeDir /r/corals/Nitya/pipelines/star_output/genome_indices \
+        --sjdbGTFfeatureExon exon \
+        --sjdbGTFtagExonParentTranscript Parent \
+        --sjdbGTFfile /r/corals/Nitya/pipelines/annotations/Pacuta.exons.GFFannotation.gff \
+        --twopassMode Basic \
+        --twopass1readsN -1 \
+        --outStd Log BAM_Unsorted BAM_Quant \
+        --outSAMtype BAM Unsorted \
+        --outReadsUnmapped Fastx \
+        --outFileNamePrefix /r/corals/Nitya/pipelines/star_output/aligned_reads/${ids[i]}
+    fi
+done
